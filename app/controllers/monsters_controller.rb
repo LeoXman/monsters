38e81d208db6ed7276b2708fb0686a8bb65cc262
LeoXman <antizfry@gmail.com>
2017-03-12 15:06:34 +0300

    sessions_controller

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -1,13 +1,13 @@
-class MonstersController < ApplicationController
-  load_and_authorize_resource
-  skip_authorize_resource :only => [:home, :search]
-  def home
+# Monsters
+class
+  MonstersController < ApplicationController
 
-  end
+  load_and_authorize_resource
+  skip_authorize_resource only: [:home, :search]
 
   def index
     authorize! :index, Monster
-    @letter   = Monster.letterize(params[:letter])
+    @letter = Monster.letterize(params[:letter])
     @monsters = Monster.letter(@letter).page(1).per(10)
   end
 
@@ -40,7 +40,10 @@ class MonstersController < ApplicationController
     if check_own?(@monster.own)
       respond_to :html
     else
-      redirect_to :back, flash: { error: 'Недостаточно прав для редактирования записи' }
+      redirect_to :back,
+                  flash: {
+                    error: 'Недостаточно прав для редактирования записи'
+                  }
     end
   end
 
@@ -52,11 +55,15 @@ class MonstersController < ApplicationController
     @monster.save
     respond_to do |format|
       if @monster.save
-        format.html { redirect_to @monster, notice: 'Monster was successfully created.' }
-        format.json { render :show, status: :created, location: @monster }
-      else
-        format.html { render :new }
-        format.json { render json: @monster.errors, status: :unprocessable_entity }
+        format.html do
+          redirect_to @monster,
+                      notice: 'Monster was successfully created.'
+        end
+        format.json do
+          render :show,
+                 status: :created,
+                 location: @monster
+        end
       end
     end
   end
@@ -67,19 +74,17 @@ class MonstersController < ApplicationController
     @monster = Monster.update(monster_params)
     respond_to do |format|
       if @monster.update(monster_params)
-        format.html { redirect_to @monster, notice: 'Monster was successfully updated.' }
-        format.json { render :show, status: :ok, location: @monster }
-      else
-        format.html { render :edit }
-        format.json { render json: @monster.errors, status: :unprocessable_entity }
+        format.html do
+          redirect_to @monster,
+                      notice: 'Monster was successfully updated.'
+        end
+        format.json do
+          render :show,
+                 status: :ok,
+                 location: @monster
+        end
       end
     end
-  end
-
-  # DELETE /monsters/1
-  # DELETE /monsters/1.json
-  def destroy
-
   end
 
   def like
@@ -88,11 +93,16 @@ class MonstersController < ApplicationController
       @monster.increment!(:like)
       vote_stamp(@monster.id)
       respond_to do |format|
-        format.json { render json: @monster, status: :created }
+        format.json do
+          render json: @monster,
+                 status: :created
+        end
       end
     else
       respond_to do |format|
-        format.json { render json: @monster, status: :error }
+        format.json do
+          render json: @monster, status: :error
+        end
       end
     end
   end
@@ -102,9 +112,15 @@ class MonstersController < ApplicationController
     if vote_stamp_check(@monster.id)
       @monster.increment!(:dislike)
       vote_stamp(@monster.id)
-      redirect_to :back, flash: { notice: 'Вы успешно проголосовали!' }
+      redirect_to :back,
+                  flash: {
+                    notice: 'Вы успешно проголосовали!'
+                  }
     else
-      redirect_to :back, flash: { error: 'Вы уже проголосовали!' }
+      redirect_to :back,
+                  flash: {
+                    error: 'Вы уже проголосовали!'
+                  }
     end
   end
 
@@ -118,52 +134,51 @@ class MonstersController < ApplicationController
     favorite.monster_name = @monster.name
     favorite.save
     respond_to do |format|
-      format.json { render json: @monster, status: :created }
+      format.json do
+        render json: @monster,
+               status: :created
+      end
       fix_count
     end
   end
 
   private
 
-    def check_own?(id)
-      @current_user.id == id
-    end
+  def check_own?(id)
+    @current_user.id == id
+  end
 
-    def fix_count
-      user = User.find(current_user.id)
-      user.fav_count = user.fav_count + 1
-      user.save
-    end
+  def fix_count
+    user = User.find(current_user.id)
+    user.fav_count = user.fav_count + 1
+    user.save
+  end
 
-    def vote_stamp_check(monster)
-      user = current_user.id
-      user = user.to_s
-      @monster = Monster.find(monster)
-      stamps = @monster.vote_stamps.to_s
-      if stamps.include? user
-        return false
-      else
-        return true
-      end
-    end
+  def vote_stamp_check(monster)
+    user = current_user.id
+    user = user.to_s
+    @monster = Monster.find(monster)
+    stamps = @monster.vote_stamps.to_s
+    return false if stamps.include? user
+    return true unless stamps.include? user
+  end
 
-    def vote_stamp(monster)
-      user = current_user.id
-      user = user.to_s
-      @monster = Monster.find(monster)
-      stamps = @monster.vote_stamps
-      stamps = stamps.to_s
-      @monster.vote_stamps = stamps + "," + user
-      @monster.save
-    end
+  def vote_stamp(monster)
+    user = current_user.id
+    user = user.to_s
+    @monster = Monster.find(monster)
+    stamps = @monster.vote_stamps
+    stamps = stamps.to_s
+    @monster.vote_stamps = stamps + ',' + user
+    @monster.save
+  end
 
-    # # Use callbacks to share common setup or constraints between actions.
-    # def set_monster
-    #   @monster = Monster.find(params[:id])
-    # end
+  # # Use callbacks to share common setup or constraints between actions.
+  # def set_monster
+  #   @monster = Monster.find(params[:id])
+  # end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def monster_params
+  def monster_params
     params.require(:monster).permit(
       :name,
       :hp,
@@ -178,10 +193,11 @@ class MonstersController < ApplicationController
       :scale,
       :own
     )
-    end
-    def comment_params
+  end
+
+  def comment_params
     params.require(:monster).permit(
       :text
     )
-    end
+  end
 end
