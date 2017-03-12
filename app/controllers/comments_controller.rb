0e122c5dd136da0a385dadd59d2db72077172694
LeoXman <antizfry@gmail.com>
2017-03-12 15:24:08 +0300

    comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,11 @@
+# Комментарии к монстрам
 class CommentsController < ApplicationController
   def create
     @monster = Monster.find(params[:monster_id])
     comment = @monster.comments.new(comment_params)
     comment.title = @current_user.username
     comment.save
-
     redirect_to monster_path(@monster)
-  end
-
-  def destroy
-
   end
 
   private
