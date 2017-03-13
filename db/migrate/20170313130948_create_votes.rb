a0a354405aaaeb8d949c0b7249cb82f9b02eef7c
LeoXman <antizfry@gmail.com>
2017-03-13 20:07:48 +0300

    Cleaning trash

--- db/migrate/20170313130948_create_votes.rb
+++ db/migrate/20170313130948_create_votes.rb
@@ -1,7 +1,8 @@
 class CreateVotes < ActiveRecord::Migration
   def change
     create_table :votes do |t|
-      t.
+      t.boolean :like
+      t.boolean :dislike
       t.belongs_to :user
       t.timestamps null: false
     end
